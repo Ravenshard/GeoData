c4ff81e984c3675978a66ad9b4ee1c035a512369
Ravenshard <jechmila@ualberta.ca>
2019-06-21 15:52:16 +0200

    adjouter le documentation

--- programs/readdata.py
+++ programs/readdata.py
@@ -8,6 +8,13 @@ import sqlite3
 dbName = "ecrains.db"
 
 def executeArgs():
+    ''' Execute all arguments from the command line before running the program
+    parameters:
+        None
+
+    Returns:
+        None
+    '''
     index = 1
     while index < len(sys.argv):
 
@@ -20,23 +27,14 @@ def executeArgs():
         index += 1
     return
 
-def getFileArg():
-    '''
-    '''
-
-    if len(sys.argv) < 2:
-        print("ERROR: not enough argv")
-        sys.exit()
-    file = sys.argv[1]
-    if not os.path.exists(file):
-        print("ERROR: file doesn't exist")
-        sys.exit()
-    return file
-
 def lireFile(file):
+    ''' Lire le file avec la donnée adjouter le base de donnée
+    Parameters:
+        Aucun
+
+    Returns:
+        Aucun
     '''
-    '''
-    # file = getFileArg()
     fileObj = open(file)
     content = fileObj.read()
     dataList = list()
@@ -48,12 +46,15 @@ def lireFile(file):
         headers = next(fileContent)
         data = collections.namedtuple(name, headers)
         for row in fileContent: dataList.append(data(*row))
-    # print(dataList)
-    # openDatabase(dataList)
     return (dataList, name, headers)
 
 def addData(dataPack):
-    '''
+    ''' Add data to the existing db
+    Parameters:
+        dataPack (tuple):   All the data extracted from a file to add to the db
+
+    Returns:
+        None
     '''
     global dbName
     dataList, name, headers = dataPack
@@ -62,6 +63,7 @@ def addData(dataPack):
         cursorObj = dbObj.cursor()
         cursorObj.execute('SELECT name from sqlite_master where type= "table"')
         tables = cursorObj.fetchall()
+        # Handle overwriting tables
         if tables != []:
             for tup in tables:
                 if name in tup:
@@ -69,18 +71,14 @@ def addData(dataPack):
                     confirmation = input("y/n?\n>")
                     if not confirmation == 'y': return
                     cursorObj.execute("DROP TABLE IF EXISTS {}; ".format(name))
-        # tableTypes = '{} char(150), ' * (len(headers)-1)
-        # tableTypes = tableTypes[:-2].format(*headers)
         tableTypes = '{} char(150), ' * (len(headers))
         tableTypes = tableTypes[:-2].format(*headers)
-        # newTable = 'create table {} ( UID integer primary key, {});'.format(name,tableTypes)
         newTable = 'create table {} ({});'.format(name, tableTypes)
         cursorObj.execute(newTable)
         values = '?,' * len(headers)
         statement = 'insert into {} values ({})'.format(name, values[:-1])
         cursorObj.executemany(statement, dataList)
     except Exception as inst:
-        # print(sys.exc_info()[0])
         print(type(inst))
         print(inst)
         input("> ")
@@ -91,7 +89,12 @@ def addData(dataPack):
     return
 
 def mainmenu():
-    '''
+    ''' Print the main menu of options available to the user
+    Parameters:
+        None
+
+    Returns:
+        None
     '''
     print("What do you want to do? Options:")
     print("(vat) view all tables, (ss) sql select, (h) help, (q) quit")
@@ -107,7 +110,12 @@ def mainmenu():
     return
 
 def selectStatement():
-    '''
+    ''' User inputs a select statement from cmd line that the program executes
+    Parameters:
+        None
+
+    Returns:
+        None
     '''
     global dbName
 
@@ -125,18 +133,27 @@ def selectStatement():
             # cursorObj.execute('select ? from refuge', (sel))
             cursorObj.execute("select {} from {}".format(sel, frm))
         else:
-            statement = 'SELECT ? FROM ? WHERE ?;'
+            # statement = 'SELECT ? FROM ? WHERE ?;'
             cursorObj.execute("select {} from {} where {}".format(sel, frm, whr))
             # cursorObj.execute(statement, [sel, frm, whr])
         tables = cursorObj.fetchall()
         print("Results:\n{}".format(tables))
+    except Exception as inst:
+        print(type(inst))
+        print(inst)
+        input("> ")
     finally:
         cursorObj.close()
         dbObj.close()
     return
 
 def viewTables():
-    '''
+    ''' View all the tables in the current database
+    Parameters:
+        None
+
+    Returns:
+        None
     '''
     global dbName
     try:
@@ -154,7 +171,6 @@ def viewTables():
 def main():
     executeArgs()
     mainmenu()
-
     return
 
 if __name__ == '__main__':
