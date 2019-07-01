c263d1d176e85200993facbc7220ddc8c1e944d4
Ravenshard <jechmila@ualberta.ca>
2019-07-01 12:28:00 +0200

    break time

--- programs/readdata.py
+++ programs/readdata.py
@@ -2,19 +2,16 @@
     Cr
 '''
 
-import os, re, sys, time, unicodedata, math, csv, collections
+import os, re, sys, time, unicodedata, math, csv, collections, locale
 import psycopg2
-
-dbName = "ecrains.db"
+from PIL import Image
 
 def getConnection():
     '''
     '''
     connect_str = "dbname='ecrains' user='Jay' host='localhost' " + \
                   "password='inriaTravail19'"
-    # use our connection values to establish a connection
     conn = psycopg2.connect(connect_str)
-    # create a psycopg2 cursor that can execute queries
     cursor = conn.cursor()
     return (conn, cursor)
 
@@ -40,6 +37,12 @@ def executeArgs():
             if os.path.exists(sys.argv[index]):
                 newData = lireFile(sys.argv[index])
                 addData(newData)
+        elif sys.argv[index] == '-del':
+            index += 1
+            deleteData(sys.argv[index])
+        elif sys.argv[index] == '-apf':
+            index +=1
+            if os.path.exists(sys.argv[index]):  addPhotoFile(sys.argv[index])
         index += 1
     return
 
@@ -64,6 +67,38 @@ def lireFile(file):
         for row in fileContent:  dataList.append(data(*row))
     return (dataList, name, headers)
 
+def addPhotoFile(file):
+    try:
+        allPhotos = os.listdir(file)
+        dbObj, cursorObj = getConnection()
+
+        cursorObj.execute('SELECT tablename FROM pg_catalog.pg_tables;')
+        tables = cursorObj.fetchall()
+        # Handle overwriting tables
+        bool = True
+        for tup in tables:
+            if 'photos' in tup:
+                bool = False
+        if bool:
+            statement = "CREATE TABLE Photos (uid text, picid text, ext text, photo bytea, primary key (uid, picid) );"
+            cursorObj.execute(statement)
+
+        for photo in allPhotos:
+            # 1165.P01.png
+            (uid, picid, ext) = photo.split('.')
+            binPhoto = open("{}.\\{}".format(file,photo), 'rb').read()
+            statement = "INSERT INTO Photos(uid, picid, ext, photo) VALUES (%s, %s, %s, %s)"
+            cursorObj.execute(statement, (uid, picid, ext, psycopg2.Binary(binPhoto)))
+    except Exception as inst:
+        print(type(inst))
+        print(inst)
+        input("> ")
+    finally:
+        dbObj.commit()
+        closeConnection(dbObj, cursorObj)
+        print("Photos added successfully")
+    return
+
 def addData(dataPack):
     ''' Add data to the existing db
     Parameters:
@@ -72,7 +107,6 @@ def addData(dataPack):
     Returns:
         None
     '''
-    global dbName
     dataList, name, headers = dataPack
     try:
         dbObj, cursorObj = getConnection()
@@ -92,7 +126,6 @@ def addData(dataPack):
         cursorObj.execute(newTable)
         values = '%s,' * len(headers)
         statement = 'insert into {} values ({})'.format(name, values[:-1])
-        print(statement)
         cursorObj.executemany(statement, dataList)
     except Exception as inst:
         print(type(inst))
@@ -101,48 +134,66 @@ def addData(dataPack):
     finally:
         dbObj.commit()
         closeConnection(dbObj, cursorObj)
+        print("Table added successfully")
     return
 
-def mainmenu():
-    ''' Print the main menu of options available to the user
-    Parameters:
-        None
-
-    Returns:
-        None
-    '''
-    print("What do you want to do? Options:")
-    print("(vat) view all tables, (ss) sql select, (h) help, (q) quit")
-    choice = ''
-    while choice != 'q':
-        choice = input("What do you want to do?\n> ")
-        if choice == 'vat':
-            viewTables()
-        elif choice == 'ss':
-            selectStatement()
-        elif choice == 'th':
-            tableHeaders()
+def deleteData(tableName):
+    try:
+        dbObj, cursorObj = getConnection()
+        cursorObj.execute("drop table if exists {}".format(tableName))
+    except Exception as inst:
+        print(type(inst))
+        print(inst)
+        input("> ")
+    finally:
+        dbObj.commit()
+        closeConnection(dbObj, cursorObj)
+        print("Table dropped successfully")
     return
 
 def testStatement():
     print("TEST:")
     dbObj, cursorObj = getConnection()
 
-    sel = 'failure'
+    sel = 'website'
     frm = 'reseau_social'
-    sql = "SELECT %s FROM reseau_social"
-    id = 1752
-    # cursorObj.execute(sql, (sel,))
+    whr = 'uid'
+    uid = '647'
+    picid = 'P02'
+    ext = 'png'
 
-    cursorObj.execute("select website from reseau_social where uid = %s", ("1752",))
-    # cursorObj.execute("select uid from reseau_social")
-    # cursorObj.execute("select {} from {}".format(sel, frm))
-
+    # cursorObj.execute("select {} from {} where {}=%s".format(sel, frm, whr), (id,))
+    statement = "SELECT * FROM photos WHERE uid ='647';"
+    cursorObj.execute(statement)
     tables = cursorObj.fetchall()
     print("Results:\n{}".format(tables))
+
+    fileNames = list()
+
+    for tup in tables:
+        fname = "{}.{}.{}".format(tup[0], tup[1], tup[2])
+        fileNames.append(fname)
+        file = open(fname, 'wb').write(tup[3])
+        im = Image.open(fname).show()
+        os.remove(fname)
+
     closeConnection(dbObj, cursorObj)
     sys.exit()
     return
+
+def tableHeaders():
+    tables = list()
+    try:
+        dbtable = input("input the table you want to view the headers of\n>")
+        dbObj, cursorObj = getConnection()
+        cursorObj.execute("SELECT column_name FROM information_schema.columns WHERE table_name = %s", (dbtable,))
+        tables = cursorObj.fetchall()
+    except Exception as inst:
+        print(type(inst))
+        print(inst)
+        input("> ")
+    finally:  closeConnection(dbObj, cursorObj)
+    return tables
 
 def selectStatement():
     ''' User inputs a select statement from cmd line that the program executes
@@ -152,40 +203,32 @@ def selectStatement():
     Returns:
         None
     '''
-    global dbName
-
     print("There are 3 parts: SELECT, FROM, WHERE")
     sel = input("input the SELECT part\n>")
     frm = input("input the FROM part\n>")
     whr = input("input the WHERE part\n>")
-
+    tables = list()
     try:
-        # dbObj = sqlite3.connect(dbName)
-        # cursorObj = dbObj.cursor()
         dbObj, cursorObj = getConnection()
         if whr == '':
-            # statement = 'SELECT ? FROM ? '
-            # cursorObj.execute(statement, [sel, frm])
-            # cursorObj.execute('select ? from refuge', (sel))
             cursorObj.execute("select {} from {}".format(sel, frm))
-            # statement = "select (%s) from {}".format(frm)
-            # print(statement)
-            # cursorObj.execute(statement, (sel,))
         else:
-            # statement = 'SELECT ? FROM ? WHERE ?;'
             cursorObj.execute("select {} from {} where {}".format(sel, frm, whr))
-            # cursorObj.execute(statement, [sel, frm, whr])
         tables = cursorObj.fetchall()
-        print("Results:\n{}".format(tables))
     except Exception as inst:
         print(type(inst))
         print(inst)
         input("> ")
-    finally:
-        closeConnection(dbObj, cursorObj)
-        # cursorObj.close()
-        # dbObj.close()
+    finally:  closeConnection(dbObj, cursorObj)
+    return tables
+
+def selectPhotos():
+    try:
+        dbObj, cursorObj = getConnection()
+    except Exception as exc:
+        print("oh no")
     return
+
 
 def viewTables():
     ''' View all the tables in the current database
@@ -195,24 +238,45 @@ def viewTables():
     Returns:
         None
     '''
-    global dbName
+    returnList = list()
     try:
-        # dbObj = sqlite3.connect(dbName)
-        # cursorObj = dbObj.cursor()
         dbObj, cursorObj = getConnection()
         cursorObj.execute('SELECT tablename FROM pg_catalog.pg_tables;')
         tables = cursorObj.fetchall()
-        returnList = list()
         for tuple in tables:
             for entry in tuple:
                 if not ("pg_" in entry or "sql_" in entry): returnList.append(entry)
+    finally:  closeConnection(dbObj, cursorObj)
+    return returnList
 
-        print("current tables:\n{}".format(returnList))
-    finally:
-        closeConnection(dbObj, cursorObj)
-        # cursorObj.close()
-        # dbObj.close()
+def mainmenu():
+    ''' Print the main menu of options available to the user
+    Parameters:
+        None
 
+    Returns:
+        None
+    '''
+    print("\nWhat do you want to do? Options:")
+    print("(vat) view all tables, (ss) sql select, (sp) select photo, (th) table headers, (q) quit")
+    choice = ''
+    while choice != 'q':
+        choice = input("\nWhat do you want to do?\n> ")
+        if choice == 'vat':
+            res = viewTables()
+            printResults(res)
+        elif choice == 'ss':
+            res = selectStatement()
+            printResults(res)
+        elif choice == 'th':
+            res = tableHeaders()
+            printResults(res)
+    return
+
+def printResults(tup):
+    '''
+    '''
+    print("Results:\n{}".format(tup))
     return
 
 def main():
